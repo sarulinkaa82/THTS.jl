25bac7541ed68a54a21be74ec1ad6e4fb2168711
sarulinkaa82 <sarka.psen@seznam.cz>
2024-11-08 09:47:26 +0100

    Added testing domain and basic testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
 using THTS
 using Test
 
-@testset "THTS.jl" begin
-    # Write your tests here.
+@testset "action choices" begin
+    include("MausamKolobov.jl")
+    m = MausamKolobov()
+    fhm = fixhorizon(m, 15)
+    solver = THTSSolver(1.4, iterations = 100)
+    
+    all_s = stage_states(fhm, 1)
+    all_states = collect(all_s)
+
+    state_test1 = all_states[1]
+    act1 = base_thts(fhm, state_test1, 100, 1.4)
+    @test act1 == "a00"
+
+    state_test2 = all_states[3]
+    act2 = base_thts(fhm, state_test2, 100, 1.4)
+    @test act2 == "a20"
+
+    state_test3 = all_states[5]
+    act3 = base_thts(fhm, state_test3, 100, 1.4)
+    @test act3 == "a41"
 end
